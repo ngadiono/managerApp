3c7871763d84975aa20405a2aac9103bb0c30be1
ngadiono <ondoelratz@gmail.com>
2018-07-26 14:56:28 +0700

    +firebase

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyD8_xYxpHhBAD__Cqb0a1cMr3IJx4zH8Ws",
+      authDomain: "managerapp-a4db0.firebaseapp.com",
+      databaseURL: "https://managerapp-a4db0.firebaseio.com",
+      projectId: "managerapp-a4db0",
+      storageBucket: "",
+      messagingSenderId: "227299538652"
+    };
+    firebase.initializeApp(config);
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    )
+  }
+}
+
+export default App;
