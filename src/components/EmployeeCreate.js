a5b25128387780f453698d36c7046d5d8a0043b8
ngadiono <ondoelratz@gmail.com>
2018-07-30 12:23:51 +0700

    add picker components

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Picker } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Picker
+            style={{ flex: 1 }}
+            selectedValue={this.props.shift}
+            onValueChange={day => this.props.employeeUpdate({ prop: 'shift', value: day })}
+          >
+            <Picker label="Monday" value="Monday" />
+            <Picker label="Tuesday" value="Tuesday" />
+            <Picker label="Wednesday" value="Wednesday" />
+          </Picker>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
